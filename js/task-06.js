84d4c0681a68581e00fb846150b792f135db6325
YuriyVolot <58395375+YuriyVolot@users.noreply.github.com>
2020-06-25 20:58:58 +0300

    0

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,3 @@
-// const inputRef = document.querySelector('#validation-input');
-// const validationInput = inputRef.dataset.length;
-
-// function borderPainter(event) {
-//     return event.target.value.length == validationInput
-//         ? inputRef.setAttribute('class', 'valid')
-//         : inputRef.setAttribute('class', 'invalid');
-// }
-
-// inputRef.addEventListener('blur', borderPainter);
-
 const inputRef = document.querySelector('#validation-input');
 const validationInput = inputRef.dataset.length;
 
--- js/task-07.js
+++ js/task-07.js
@@ -1,12 +1,3 @@
-// const inputRef = document.querySelector('#font-size-control');
-// const textRef = document.querySelector('#text');
-
-// function fontSizeChange() {
-//     textRef.setAttribute('style', `font-size: ${inputRef.value}px`);
-// }
-
-// inputRef.addEventListener('input', fontSizeChange);
-
 const barRef = document.querySelector('#font-size-control');
 const textRef = document.querySelector('#text');
 
